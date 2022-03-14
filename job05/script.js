0a57e54d2b4863893f473a73c9e501cacc764841
mathieu-tatat <mathieu.tatat@laplateforme.io>
2022-03-14 12:23:38 +0100

    job05

---
+++ job05/script.js
@@ -0,0 +1,9 @@
+const jourssemaines=["Lundi","Mardi","Mercredi","Jeudi","Vendredi","Samedi","Dimanche"];
+function afficherjourssemaines(){
+
+  for(j=0 ;j < jourssemaines.length; j++)
+  {
+    console.log(jourssemaines[j])
+  }
+}
+afficherjourssemaines()
