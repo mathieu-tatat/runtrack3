f191f8f7a00d9f0e376ec50a9f25b4ddbb9898ed
mathieu-tatat <mathieu.tatat@laplateforme.io>
2022-03-16 13:21:18 +0100

    job02

---
+++ jour03/job02/index.php
@@ -0,0 +1,42 @@
+<style>
+#images{
+    display:flex;
+    justify-content:center;
+    background-color:blue;
+}
+#melangees{
+    display:flex;
+    justify-content:center;
+    background-color:pink;
+}
+
+#rangees{
+    display:flex;
+    justify-content:center;
+    background-color:yellow;
+}
+</style>
+<!doctype html>
+<html lang="fr">
+<head>
+  <meta charset="utf-8">
+  <title>Arc-en-ciel</title>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script src="script.js"></script>
+</head>
+<body>
+    <main>
+    <div id="images">
+        <img src="arc1.png">
+        <img src="arc2.png">
+        <img src="arc3.png">
+        <img src="arc4.png">
+        <img src="arc5.png">
+        <img src="arc6.png">
+    </div>
+    <div id="melangees"></div>
+    <div id="rangees"></div>
+    <button id="button">Mélanger</button>
+</main>
+
+</body>
